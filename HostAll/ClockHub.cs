013780522589759109044a0d71854adb281f2425
James.Bolt <james.bolt@theaccessgroup.com>
2017-07-24 20:50:01 +0100

    first commit

---
+++ HostAll/ClockHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace HostAll
+{
+    public class ClockHub : Hub
+    {
+
+        Timer timer;
+
+        public override Task OnConnected()
+        {
+
+            if (timer == null)
+            {
+                timer = new Timer(1000);
+
+                timer.Elapsed += (s, e) =>
+                {
+                    {
+                        Clients.All.showTime(DateTime.Now.ToString("hh:mm:ss"));
+                    }
+                };
+
+                timer.Start();
+
+            }
+            return base.OnConnected();
+
+        }
+    }
+}
---
+++ HostAll/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+
+[assembly: OwinStartup(typeof(HostAll.Startup))]
+
+namespace HostAll
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
